afab8516b949362496844919199ad08e7448c117
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-03-18 20:05:20 +0100

    add LoggingAspect for controller endpoints

--- src/main/java/com/example/networkinterface/NetworkInterfaceApplication.java
+++ src/main/java/com/example/networkinterface/NetworkInterfaceApplication.java
@@ -3,9 +3,11 @@ package com.example.networkinterface;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@ComponentScan("com.example.networkinterface")
+@EnableAspectJAutoProxy
+@ComponentScan
 public class NetworkInterfaceApplication {
 
 	public static void main(String[] args) {
