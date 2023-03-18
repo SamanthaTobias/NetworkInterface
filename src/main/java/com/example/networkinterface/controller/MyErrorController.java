3f7a3cfedeb761db42cc152950ad1c9669ec0702
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-03-18 20:06:53 +0100

    log invalid statuscode in error controller

--- src/main/java/com/example/networkinterface/controller/MyErrorController.java
+++ src/main/java/com/example/networkinterface/controller/MyErrorController.java
@@ -2,6 +2,8 @@ package com.example.networkinterface.controller;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyErrorController implements ErrorController {
+
+	private static final Logger log = LogManager.getLogger(MyErrorController.class);
 
 	@RequestMapping("/error")
 	public String handleError(HttpServletRequest request, Model model) {
@@ -21,8 +25,7 @@ public class MyErrorController implements ErrorController {
 			try {
 				statusCode = HttpStatus.valueOf(statusCodeInt);
 			} catch (IllegalArgumentException e) {
-				// Invalid status code, fallback to internal server error
-				// TODO log illegal argument
+				log.error("Invalid status code: {}, falling back to internal server error", statusCodeInt);
 			}
 		}
 
